ad33816e80127c511ced3fea900ec6352f596963
Graduation Project <project@example.com>
2025-12-28 02:04:58 +0800

    Remove unnecessary documentation files

--- fix_password.sql
+++
@@ -1,4 +0,0 @@
-USE shopping_mall;
--- BCrypt hash for password "123456" (generated with cost factor 10)
-UPDATE tb_user SET password = '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy';
-SELECT id, username, password FROM tb_user;
--- update_password.sql
+++
@@ -1,3 +0,0 @@
-USE shopping_mall;
-UPDATE tb_user SET password = '$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HZWzG3YB1tlRy.fqvM/BG';
-SELECT id, username, password FROM tb_user;
